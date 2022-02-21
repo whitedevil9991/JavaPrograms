c2b9beaf29c07b00aadf037add6d025eb4307733
whitedevil9991 <revanth.pilli@yahoo.com>
2022-02-21 21:20:55 +0530

    master

---
+++ java programs/functions.java
@@ -0,0 +1,16 @@
+public class functions {
+    public int addition(int n1,int n2, int sum) {
+        
+        sum=n1+n2;
+        System.out.println("subtraction is :"+sum);
+        return sum;
+    }
+    public void sub() {
+        int n1=100,n2=400,sum;
+        sum=n1+n2;
+        System.out.println("subtraction is :"+sum);
+    }
+    public static void main(String[] args) {
+
+    }
+}
