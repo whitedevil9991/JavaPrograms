8b152cb090158dee3ecafc4e6db12b3ae43f8471
whitedevil9991 <revanth.pilli@yahoo.com>
2022-03-01 15:17:02 +0530

    master

--- java programs/DemoInheritance.java
+++ java programs/DemoInheritance.java
@@ -3,7 +3,7 @@ import javax.tools.ForwardingFileObject;
 public class DemoInheritance 
 {
     public class Animal 
-    {
+    { //here you are declaring three things below under private access modifiers
         private String food_type;
         private int no_of_legs;
         private String type;
@@ -17,13 +17,19 @@ public class DemoInheritance
 
     public Animal(String  f,int n,String t)
     {
+        //this is a constructor with parameters
         food_type="Omnivore";
         no_of_legs=4;
         type="mammal";
     }
+    public String toString() 
+    {
+        return "food type:"+food_type+"no of legs"+no_of_legs+"type"+type;
+    }
 
     public static void main(String[] args) 
-    {
+    {//this is the main method
+        return "food type"+food_type+"no of legs"+no_of_legs+"type"+type;
         //TODO auto-generated method stub
     }
     
