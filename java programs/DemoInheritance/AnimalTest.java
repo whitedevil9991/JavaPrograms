c7bf697cf6d583070a5c2ba8b06c3c5c530895bc
whitedevil9991 <revanth.pilli@yahoo.com>
2022-03-01 15:54:21 +0530

    master

--- java programs/DemoInheritance/AnimalTest.java
+++ java programs/DemoInheritance/AnimalTest.java
@@ -1,3 +1,15 @@
-public class AnimalTest {
+public class AnimalTest 
+{
+    public static void main(String[] args)
+    {
+       
+        Animal a1=new Animal(); 
+        // creating a object using previous Animal program named a1
+        System.out.println(a1);
+
+        Bird a2=new Bird(); 
+        //creating a object using previous Animal program named a2
+        System.out.println(a2);
+    }
     
 }
