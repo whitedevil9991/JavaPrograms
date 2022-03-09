3b86f7647fd52bec63a950d68e7ece9bcdc5afe9
whitedevil9991 <revanth.pilli@yahoo.com>
2022-03-09 21:41:04 +0530

    mastert

--- java programs/CollectionPackage/ListDemo.java
+++ java programs/CollectionPackage/ListDemo.java
@@ -1,26 +1,37 @@
 public class ListDemo {
-    /*collection framework = group of objects (to avoid the drawback of arrays)
+    /*Collection Framework = group of objects (to avoid the drawback of arrays)
     alll data strucutres classes are the part of collection framework
     Draw back of array: array has fixed sizes
     collection--Dynamic collection of objects (works on entire collection)
 
     java.util ( all collection related packages are in here, so import this to use collection packages)
-    --Collection--
+    --Collection--(--IMP-part--)(related to core java)
     1. List-can store duplicate values, ordered collection
     (It is has index so it is known as ordered collection)(list of MobileNumbers,Students etc)
-    classes---Vector, ArrayList, LinkedList, Stack(builtin-classes)
+    built-in-classes---Vector(thread-safe), ArrayList(not thread-safe), LinkedList(scattered-format), Stack((LIFO)last-in first-out)
 
     2. Set-can store unique values, unordered collection 
     (storing only unique values) (set doesn't have index so unordered collection)(the output comes in unordered way)
-    classes--HashSet
+    classes---HashSet(sub-interfaces) (for ascending or descending)
     SortedSet-in ascending order
-    NevigableSet-seraching
-    classes---Treeset
+    NevigableSet--for seraching
+    classes---Treeset(for automatic sorting)
 
-    3. Queue-setting priority
-    (this is also a list but priority based)
-    Deque-double ended queue
+    3. Queue-setting priority 
+    (this is also a list-type but priority based)
+    Deque-double ended queue (sub-interfaces)
     classes---PriorityQueue
+
+
+    Map-key value pair (to-store two values like EmployeeName-EmployeeID)
+    classes--- HashMap 
+    SortedMap-sorted map
+    NevigableMap-searching
+    classes--- TreeMap
+
+    Comparator and comparable---used for comparison of objects
+
+    Iterator-traversing
     */
     
 }
