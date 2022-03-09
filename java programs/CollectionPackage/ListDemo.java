86175273f8b0314223446703e64123849324303f
whitedevil9991 <revanth.pilli@yahoo.com>
2022-03-09 20:29:33 +0530

    master

---
+++ java programs/CollectionPackage/ListDemo.java
@@ -0,0 +1,26 @@
+public class ListDemo {
+    /*collection framework = group of objects (to avoid the drawback of arrays)
+    alll data strucutres classes are the part of collection framework
+    Draw back of array: array has fixed sizes
+    collection--Dynamic collection of objects (works on entire collection)
+
+    java.util ( all collection related packages are in here, so import this to use collection packages)
+    --Collection--
+    1. List-can store duplicate values, ordered collection
+    (It is has index so it is known as ordered collection)(list of MobileNumbers,Students etc)
+    classes---Vector, ArrayList, LinkedList, Stack(builtin-classes)
+
+    2. Set-can store unique values, unordered collection 
+    (storing only unique values) (set doesn't have index so unordered collection)(the output comes in unordered way)
+    classes--HashSet
+    SortedSet-in ascending order
+    NevigableSet-seraching
+    classes---Treeset
+
+    3. Queue-setting priority
+    (this is also a list but priority based)
+    Deque-double ended queue
+    classes---PriorityQueue
+    */
+    
+}
