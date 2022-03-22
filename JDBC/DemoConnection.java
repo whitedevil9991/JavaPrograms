e7f636d2ac43dde73b95103c703b809912498778
whitedevil9991 <revanth.pilli@yahoo.com>
2022-03-23 00:21:45 +0530

    master

--- JDBC/DemoConnection.java
+++ JDBC/DemoConnection.java
@@ -1,6 +1,26 @@
+import java.sql.DriverManager;
+import java.sql.*;
 public class DemoConnection {
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) 
+    
+    {
+
+        try 
+        {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        //Class.forName("com.mysql.jdbc.Driver");
+        Connection =con DriverManager.getConnection("jdbc:oracle:think:@localhost:1521:orc1\r\n","system","oracle99");
+        //system = user id,  pwd = oracle99
+        //Connection = con DriverManager.getConnection("jdbc.mysql:")
+
+        System.out.println("connected with database");
+
+        } /*catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }*/
     }
 }
--- java programs/JavaStrings/StringClassDemo.java
+++ java programs/JavaStrings/StringClassDemo.java
@@ -20,6 +20,8 @@ public class StringClassDemo {
      * s2.contains("keyword") = contains checks for the "keyword in s2" and returns a boolean value
      * s2.substring(2,5) = prints the stringValues from index 2 to 5 in s2
      * s2.replace('w','c') = replaces the 'w' with 'c' in s2
+     * s2.length() = shows s2 length
+     * 
      * 
      */
 
@@ -36,6 +38,7 @@ public class StringClassDemo {
 
          String t1=s1.concat(s2);
          System.out.println(t1);
+
          int l=s2.length();
          System.out.println(l);
 
@@ -46,19 +49,23 @@ public class StringClassDemo {
 
          s=s2.replace('w','c');
          System.out.println(s);
+
          s=s.toLowerCase(); // not present in String buffer and building
          s=s.toUpperCase(); //same here
 
          //getChars(int srcBegin, int srcEnd, char[] dst, int dstBegin);
          String s3="this is String";
          char[] s4=new char[7];
+
+         //getChars() = saves the elements in the char
          s3.getChars(3,9,s4,0);
          System.out.println(s4);
+
          //jdk 8 new Methods of String
          String j1=String.join(":","football","foot","ball");
          System.out.println(j1);
 
-         String j2=j1.chars().distinct().mapToObj(c->String.value);
+         String j2=j1.chars().distinct().mapToObj(c->String.valueOf);
          System.out.println(j2);
 
          StringBuffer s11=new StringBuffer();
@@ -72,8 +79,10 @@ public class StringClassDemo {
          s11.insert(7,"pen");
          System.out.println("inserted"+s11);
 
-         s11.reverse();;
+         s11.reverse();
          System.out.println("reversed"+s11);
+
+         StringBuilder s12=new StringBuilder("this is string builder");
 
      }
 }
