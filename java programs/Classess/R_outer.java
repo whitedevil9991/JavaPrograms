404f9fb5510f3649d48361bd7d5fe83e00bfbfee
Pilli Revant <63239618+whitedevil9991@users.noreply.github.com>
2024-04-08 19:45:52 +0530

    Update R_outer.java

--- java programs/Classess/R_outer.java
+++ java programs/Classess/R_outer.java
@@ -1,29 +1,24 @@
 import DemoInheritance.InnerClass.R_outer.RInner;
-
 public class InnerClass {
     /**inner classes only accessible through outer class so more secure
      * they are hidden and cannot be accessed by outside
      * outer class private members are directly accessible by inner class
      * where as the vice versa is not possible
-     * 
      * inner classes are secure as they are only accessible through outer class
      * Types of inner class :
      * 1)Regular inner class
      * 2)Static inner class 
      * 3)Method local inner class
      * 4)Ananymous inner class: anonymous inner classes are replaced by lamda expression in js
-     * 
      * Regular inner class=a class inside a class
      * one outer class object can have multiple inner class objects
-     * 
-     * 
      */
-    public class R_outer //regular inner class
+    //regular inner class
+    public class R_outer 
     {
         private int o;
-
-        public R_outer()  //outer class
-        {
+        //outer class
+        public R_outer(){
             o=100;
         }
         public R_outer(int o1){
@@ -32,10 +27,9 @@ public class InnerClass {
         public void RODisplay() {
             System.out.println("im Router"+o);
         }
-        class RInner //inner class
-        {
+        //inner class
+        class RInner {
             private int i;
-
             public RInner(){
                 i=230;
             }
@@ -50,11 +44,9 @@ public class InnerClass {
     public static void main(String[] args) {
         R_outer r=new R_outer();
         r.RODisplay();
-
         RInner in=r.new RInner();
         in.RIDisplay();
         RInner in2=r.new RInner();
         in2.RODisplay();
     }
-    
 }
